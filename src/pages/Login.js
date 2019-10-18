057650fe31f0eaa9425e77db0594d7a60d9a41f7
Carson Bunnell <carsonbunnell25@gmail.com>
2019-10-17 19:16:04 -0600

    i don't even know

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,28 +2,75 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Login.css';
 
-class Login extends Component {
-    state = {
-        user_handle: '',
-        password: ''
-    }
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email : '',
+      password: ''
+    };
+  }
 
-    handleInput = () => {
+  onChangeEmail = (e) => {
+      this.setState({
+          email: e.target.value
+      })
+  };
 
-    }
+  onChangePassword = (e) => {
+    this.setState({
+        password: e.target.value
+    })
+};
 
-    render() {
-        return (
-            <div className="login-container">
-                <input className="username-input" name="user_handle" value={this.state.user_handle} placeholder="user_handle" />
-                <input className="password-input" name="user_handle" value={this.state.user_handle} placeholder="password" />
-                <div className="login-btn-container">
-                    <button className="login-btn">Login</button>
-                    <div className="needs-account">need an account? <Link className="sign-up-link" to='/signup'>sign up</Link></div>
-                </div>
-            </div>
-        )
-    }
+  onSubmit = (event) => {
+    event.preventDefault();
+    fetch('/users/authenticate', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        this.props.history.push('/');
+      } else {
+        const error = new Error(res.error);
+        throw error;
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      alert('Error logging in please try again');
+    });
+  }
+
+  render() {
+    return (
+        <div className="login-container">
+          
+        <input
+            type="email"
+            className="username-input"
+            placeholder="email"
+            value={this.state.email}
+            onChange={this.onChangeEmail}
+            required
+        />
+        <input
+            type="password"
+            className="password-input"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.onChangePassword}
+            required
+        />
+        <div className="login-btn-container">
+        <button className="login-btn" onClick={this.onSubmit}>Login</button>
+        <div className="needs-account">need an account? <Link className="sign-up-link" to='/signup'>sign up</Link></div>
+        </div>
+        </div>
+    );
+  }
 }
-
-export default Login;
