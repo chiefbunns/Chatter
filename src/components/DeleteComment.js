ed62ab0f1c98aae7232c919107b043bcdfe71c0b
Kyle Norton <kylebnorton@gmail.com>
2019-10-28 11:00:27 -0600

    Tinkering with more actions button

--- src/components/DeleteComment.js
+++ src/components/DeleteComment.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import CommentActions from './CommentActions';
 import { FaMeteor } from 'react-icons/fa';
 import more from '../svgs/more.svg';
 //import { TiTrash } from 'react-icons/ti';
 import axios from 'axios';
-import '../css/Comment.css';
+import '../css/CommentActions.css';
 
 
 class DeleteComment extends Component {
@@ -32,9 +33,9 @@ class DeleteComment extends Component {
 
   render() {
     return (
-      <div title="destroy" className="delete-comment">
-        {!this.state.options ? <div onClick={this.toggleOptions} ><img src={more} alt="More Icon" /></div> :
-          <FaMeteor title="destroy" size="17px" color="#E84855" onClick={this.deleteComment} />}
+      <div title="destroy">
+        {!this.state.options ? <div className="more-ations-btn" onClick={this.toggleOptions} ><img src={more} alt="More Icon" /></div> :
+          <div className="more-actions"><CommentActions /><FaMeteor title="destroy" size="17px" color="#E84855" onClick={this.deleteComment} /></div>}
       </div>
     )
   }
